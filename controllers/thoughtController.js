0a638e6e41366e7333b56fb0e122bd483d874896
sailorshy94 <sbutle94@gmail.com>
2024-03-09 11:50:18 -0500

    created code for newThought method, used $addToSet property to add thought to array of user's thoughts, setup section for updateThought and deleteThought methods

--- controllers/thoughtController.js
+++ controllers/thoughtController.js
@@ -1,47 +1,59 @@
 const Thought = require('../models/Thought');
 
 module.exports = {
-// GET all thoughts
-async getAllThoughts(req, res) {
-    try {
-        const thoughtsData = await Thought.find();
-        res.json(thoughtsData);
-    } catch (err) {
-        res.status(500).json(err);
-    }
-},
-// GET single thought by _id
-async getOneThought(req, res) {
-    try {
-        const thought = await Thought.findOne({ _id: req.params.thoughtId })
-            .select('-__v');
-
-        if (!thought) {
-            return res.status(404).json({ message: 'No thought with that ID.' });
+    // retrieves all thoughts
+    async getAllThoughts(req, res) {
+        try {
+            const thoughtsData = await Thought.find();
+            res.json(thoughtsData);
+        } catch (err) {
+            res.status(500).json(err);
         }
-        res.json(thought);
-    } catch (err) {
-        res.status(500).json(err);
-    }
-},
-// POST new thought (IMPORTANT: push created thought's _id to assoc user's thoughts array field)
+    },
+    // retrieves single thought by _id
+    async getOneThought(req, res) {
+        try {
+            const thought = await Thought.findOne({ _id: req.params.thoughtId })
+                .select('-__v');
 
+            if (!thought) {
+                return res.status(404).json({ message: 'No thought with that ID.' });
+            }
+            res.json(thought);
+        } catch (err) {
+            res.status(500).json(err);
+        }
+    },
+    // creates new thought
+    async newThought(req, res) {
+        try {
+            const newThought = await Thought.create(req.body);
+            const user = await User.findOneAndUpdate(
+                { _id: req.body.userId },
+                // $addToSet = Mongoose docs "Adds elements to an array only if they do not already exist in the set."
+                // push created thought's _id to assoc user's thoughts array field
+                { $addToSet: { thoughts: newThought._id } },
+                { new: true }
+            );
+
+            if (!user) {
+                return res.status(404).json({
+                    message: 'Thought was added, but no user exists with that ID',
+                });
+            }
+            res.json('Thought succesfully added!!');
+        } catch (err) {
+            res.status(500).json(err);
+        }
+    },
+    // updates thought by _id
+    // async updateThought(req,res)
+
+    // DELETE remove thought by _id
+    // removeThought(req, res)
 };
 
 
-// PUT update thought by _id
 
-// DELETE remove thought by _id
 
-// structure =
-/*
-async methodName (req, res) {
-    try {
-        const = await 
-        res.json();
-    } catch (err) {
-        res.status().json(err);
-    }
-};
 
-*/
