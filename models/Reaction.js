337eef09ec2c73d205e4f3bf94e187f70c3a4e39
sailorshy94 <sbutle94@gmail.com>
2024-03-10 13:02:47 -0400

    set up reactions schema - used createdAt property from Thought model

--- models/Reaction.js
+++ models/Reaction.js
@@ -1,18 +1,30 @@
 // this is not a model ONLY A SCHEMA 
-
 const { Schema, Types } = require('mongoose');
 
-const reactionSchema = new Schema ({});
-
-// allows getters to be included w/ res
-// {
-//     toJSON: {
-//       getters: true,
-//     },
-//     id: false,
-//   }
+const reactionSchema = new Schema({
+    reactionId: {
+        type: Schema.Types.ObjectId,
+        default: () => new Types.ObjectId(),
+    },
+    reactionBody: {
+        type: String,
+        required: true,
+        maxlength: 280,
+    },
+    username: {
+        type: String,
+        required: true,
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now,
+        get: (date) => date.toISOString(),
+        // use getter method to format timestamp on query??
+    },
+    toJSON: {
+        getters: true,
+    },
+    id: false,
+});
 
 module.exports = reactionSchema;
-
-// TODO: in the Thought model file there will be a property called reactions, format as:
-// reactions: [reactionSchema], 
